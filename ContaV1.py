26325ceac5c35d8dd649ff3ec899dace8a5d560c
IlkaCarmo <ilka.scarmo@gmail.com>
2025-06-16 22:19:07 -0300

    melhoria

--- ContaV1.py
+++ ContaV1.py
@@ -13,6 +13,7 @@ saques_diarios = 3
 numero_saques = 0
 extrato = []
 
+
 while True:
     opcao_escolhida = input(menu)
 
@@ -21,7 +22,7 @@ while True:
         valor_saque = float(input())
         if valor_saque > saldo:
             print("Saldo insuficiente para saque")
-        elif saques_diarios <= numero_saques:
+        elif numero_saques  >= saques_diarios:
             print("Limite de saques diários atingido")
         elif valor_saque > limite_saque_diario:
             print("Valores de saque acima  de R$ 500,00 não são permitidos")
@@ -30,7 +31,7 @@ while True:
             saques_diarios -= 1
             numero_saques += 1
             extrato.append(f"Saque: -R$ {valor_saque:.2f}")
-            print(f"Saque de {valor_saque} realizado com sucesso. Saldo atual: {saldo:.2f}")
+            print(f"Saque de {valor_saque} realizado com sucesso.")
             print(f"Saques restantes hoje: {saques_diarios}")
 
     elif opcao_escolhida == "2":
@@ -41,14 +42,16 @@ while True:
         else:
             saldo += valor_deposito   
             extrato.append(f"Depósito: R$ {valor_deposito:.2f}")
-            print(f"Depósito de {valor_deposito} realizado com sucesso. Saldo atual: {saldo:.2f}")
+            print(f"Depósito de {valor_deposito} realizado com sucesso.")
 
     elif opcao_escolhida == "3":
-        print("Extrato:")
+        print("\n=========== EXTRATO =============")
         for transacao in extrato:
             print(f"{transacao}")
 
         print(f"Saldo atual: R$ {saldo:.2f}")
+        print("\n=================================")
+
     elif opcao_escolhida == "4":
         print("Volte sempre")
         break
